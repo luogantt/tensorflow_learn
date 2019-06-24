3184f339fa59be641c1130088c6185729673dffb
luogantt <luogantt@yahoo.com>
2019-06-24 17:32:12 +0800

    修改：     tensorflow_minit/bp.py

--- tensorflow_minit/bp.py
+++ tensorflow_minit/bp.py
@@ -10,7 +10,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist=input_data.read_data_sets('MNIST_data/',one_hot=True)
 '''
-
+#################################
 import tensorflow as tf
 
 import numpy as np
